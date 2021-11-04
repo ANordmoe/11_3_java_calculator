77404685a3ba090f11b933224928babed665ee69
Alissa Nordmoe <alissa.nordmoe@gmail.com>
2021-11-04 10:09:06 -0400

    completed calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,3 +1,28 @@
 public class Calculator {
-    
+
+    public int add(int num1, int num2) {
+        int result = num1 + num2;
+        return result;
+    }
+
+    public int subtract(int num1, int num2){
+        int result = num1 - num2;
+        return result;
+    }
+
+    public int multiply(int num1, int num2){
+        int result = num1 * num2;
+        return result;
+    }
+
+    public int divide(int num1, int num2){
+        int result = num1 / num2;
+        return result;
+    }
+
+    public int square(int num) {
+        int result = num * num;
+        return result;
+    }
+
 }
